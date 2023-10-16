b7e3704c499acf15758e04418ef4a22e72a70468
Doug <douglazenby@gmail.com>
2023-10-16 16:33:06 +0100

    Added function to output dataframes of image and mask paths

--- initial_testing.py
+++ initial_testing.py
@@ -7,41 +7,28 @@ import matplotlib.pyplot as plt
 import os
 
 
-def file_locs_and_labels(root_dir):
-    """
-    Returns a dataframe of names and class labels of all images in root_dir.
-    """
-    # List of subfolders sorted by class label, omitting secret files
-    sub_folders = sorted((f for f in os.listdir(root_dir) \
-                          if not f.startswith(".")), key=str.lower)
+def get_image_path_dfs(split):
+    for _, __, f in os.walk(os.path.join('./', split, f'{split}-org-img')):
+        df = pd.DataFrame(data=f, columns=['images'])
 
-    # List of class labels
-    labels = [int(name.split(".")[0]) for name in sub_folders]
+    df['masks'] = df['images'].str.extract("(\d{4})") + "_lab.png"
 
-    data = []
-
-    for sub_f, label in zip(sub_folders, labels):
-        for _, __, f in os.walk(os.path.join(root_dir, sub_f)):
-            for file in f:
-                if file.endswith(".jpg"):
-                    data.append((os.path.join(sub_f, file), label))
-
-    df = pd.DataFrame(data, columns=['file_name', 'label'])
+    df['images'] = f'semantic-segmentation-floodnet/{split}/{split}-org-img/' + df['images']
+    df['masks'] = f'semantic-segmentation-floodnet/{split}/{split}-label-img/' + df['masks']
 
     return df
 
 
-root_dir = './test'
-sub_folders = os.listdir(root_dir)
+splits = ['train', 'val', 'test']
+dfs = []
+for split in splits:
+    df = get_image_path_dfs(split)
+    dfs.append(df)
 
-img_paths, mask_paths = [], []
-for sub_f in sub_folders:
-    for _, __, f in os.walk(os.path.join(root_dir, sub_f)):
-        for file in f:
-            if file.endswith(".png"):
-                mask_path = os.path.join(sub_f, file)
-                mask_paths.append(mask_path)
-            else:
-                img_paths.append(os.path.join(sub_f, file))
+df_train, df_val, df_test = dfs[0], dfs[1], dfs[2]
 
-print([*zip(img_paths, mask_paths)])
+print(df_train)
+print(df_val)
+print(df_test)
+
+# TODO: write script that writes image and mask paths to CSVs for train, test and val images
